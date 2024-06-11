f5e75621d580291e9d1b2de55578587d62ae4262
Seçgin <secginsanli@gmail.com>
2024-06-12 00:03:07 +0300

    Service provider config düzenleme

--- src/AuthProvider.php
+++ src/AuthProvider.php
@@ -10,16 +10,16 @@ class AuthProvider implements ServiceProviderInterface
 {
     public function register(DiInterface $di): void
     {
-        /** @var Config $config */
-        $config = $di->getShared('config');
-        $authConfig = $config->get('auth');
-        $authConfig = $authConfig ? $authConfig->toArray() : [];
-        $authConfig['cacheDir'] = $config->path('application.cacheDir') . 'session/';
-
         $di->setShared('authPermission', Permission::class);
         $di->setShared('authSession', AuthSession::class);
         $di->setShared('authResourcePermission', ResourcePermission::class);
-        $di->setShared('auth', function() use ($authConfig) {
+        $di->setShared('auth', function() {
+            /** @var Config $config */
+            $config = $this->getShared('config');
+            $authConfig = $config->get('auth');
+            $authConfig = $authConfig ? $authConfig->toArray() : [];
+            $authConfig['cacheDir'] = $config->path('application.cacheDir') . 'session/';
+
             return new Auth($authConfig);
         });
     }
